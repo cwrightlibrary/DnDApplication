6981c2bfdb734f4702e3081fcb1ec16939f6acb2
Chris Wright <cwright@richlandlibrary.com>
2026-03-08 15:00:00 -0400

    updating races

--- src/models/races.py
+++ src/models/races.py
@@ -11,6 +11,7 @@ from src.enums.race_constants import (
     DwarfToolProficiences,
     DwarfWeaponProficiencies,
     HeightAndWeight,
+    Size,
 )
 
 
@@ -62,4 +63,42 @@ class Dwarf(BaseRace):
 class Elf(BaseRace):
     name: Literal["Elf"] = "Elf"
 
-    ability: Ability = Field(default_factory=lambda: Ability)
+    ability: Ability = Field(default_factory=lambda: Ability(dexterity=2))
+    age: AgeRange = Field(default_factory=lambda: AgeRange(mature=100, max=750))
+    darkvision: int = 60
+    skill_proficiencies: list[str] = Field(default_factory=lambda: ["Perception"])
+    language_proficiencies: list[str] = Field(
+        default_factory=lambda: ["Common", "Elvish"]
+    )
+
+
+# Gnome
+class Gnome(BaseRace):
+    name: Literal["Gnome"] = "Gnome"
+    size: Size = Field(default_factory=lambda: Size.SMALL)
+
+    speed: Annotated[int, Field(ge=10, le=40)] = 25
+    ability: Ability = Field(default_factory=lambda: Ability(intelligence=2))
+    height_and_weight: HeightAndWeight = Field(
+        default_factory=lambda: HeightAndWeight(
+            height=35, height_mod="2d4", weight=35, weight_mod="None"
+        )
+    )
+    age: AgeRange = Field(default_factory=lambda: AgeRange(mature=40, max=500))
+    darkvision: int = 60
+    language_proficiencies: list[str] = Field(
+        default_factory=lambda: ["Common", "Gnomish"]
+    )
+
+
+# Half-Elf
+class HalfElf(BaseRace):
+    name: Literal["Half-Elf"] = "Half-Elf"
+
+    speed: Annotated[int, Field(ge=10, le=40)] = 30
+    ability: Ability = Field(default_factory=lambda: Ability(charisma=2))
+    height_and_weight: HeightAndWeight = Field(default_factory=lambda: HeightAndWeight(height=57, height_mod="2d8", weight=110, weight_mod="2d4"))
+    age: AgeRange = Field(default_factory=lambda: AgeRange(mature=20, max=180))
+    darkvision: int = 60
+    language_proficiencies: list[str] = Field(default_factory=lambda: ["Common", "Elvish"])
+    
